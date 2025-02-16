d2f4ec014df1230a8828ffec51251dd7994443e3
ElDiegu <eldiegugg@gmail.com>
2025-02-16 17:17:24 +0100

    Updated UserScript metadata

--- script-compendium/script-compendium.js
+++ script-compendium/script-compendium.js
@@ -1,12 +1,11 @@
 // ==UserScript==
-// @name         War Weapons Selector
+// @name         Diegu's Script Compendium
 // @version      0.1
-// @description  Places full, half or 2/3 of usable weapons in weapons slots of wars.
+// @description  This script serves as a way to centralize the loading of all my QoL scripts
 // @author       Discord: el_diegu
 // @match        https://rivalregions.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=rivalregions.com
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @require      https://github.com/ElDiegu/diegus-scripts/raw/script/war-weapons-selector/war-weapons-selector/war-weapons-selector.js
 // ==/UserScript==
 
 const compendium_metaData = {
